d424b7e5f3642976fd6b0843ce8b37026c06bbd6
ksadmin1 <ksadmin@ks.local>
2026-07-29 07:55:35 +0000

    fin 17

--- compose/app/app.py
+++ compose/app/app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def hello():
     return "Hello Devops World"
 
+
 if __name__=="__main__":
     app.run(
-	host='0.0.0.0',
-	port=5000
+        host='0.0.0.0',
+        port=5000
     )
